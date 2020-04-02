1fbf39be676e3f102c2bfc87548340b82c9e7533
Chandra <chandratan030501@gmail.com>
2020-04-02 21:17:11 +0700

    init

---
+++ app/src/main/java/com/tpa/HelepDoc/models/Drug.kt
@@ -0,0 +1,12 @@
+package com.tpa.HelepDoc.models
+
+class Drug (name: String, indicator:String, attention: String, dosage:String,  composition:String, price:Double){
+    var name:String = name
+    var indicator:String = indicator
+    var attention:String = attention
+    var dosage:String =dosage
+    var price:Double= price
+
+
+}
+
